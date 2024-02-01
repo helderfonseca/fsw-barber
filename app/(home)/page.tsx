774c27932858d284f7f3ec3323bbde70322af4e0
Helder Fonseca <helder2391@gmail.com>
2024-02-01 13:35:43 -0100

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,24 @@
+import { format } from "date-fns"
 import Header from "../_components/header";
+import { pt } from "date-fns/locale";
 
 export default function Home() {
   return (
-    <Header />
+    <div>
+      <Header />
+
+      <div className="py-6 px-5 flex flex-col gap-1">
+
+        <h2 className="text-xl font-bold">
+          Olá, Helder!
+        </h2>
+        <p className="text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: pt
+          })}
+        </p> 
+      </div>
+    </div>
+
   );
 }
